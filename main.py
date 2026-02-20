cef944f41c383c18523a3ff0086186c5cdc003c3
mey69809-droid <meys69809@gmail.com>
2026-02-19 20:51:37 -0800

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class BankAccount :
         self.name = name
 
     #check balance
-    def chechBalance(self, secret):
+    def checkBalance(self, secret):
         if secret == self.__secret:
             print(f'{self.name}: your balance is {self.balance}')
         else:
@@ -16,9 +16,20 @@ class BankAccount :
     def deposet(self, amount, secret):
         if secret == self.__secret:
             self.balance +=amount
-            print(f"{self.name} your deposit is{amount}")
+            print(f"{self.name} your deposit is {amount}")
         else:
             print("Your secret is not correct. Please, try again!!!")
+    #withdraw
+    def withdraw(self, secret,balance, amount):
+        if secret == self.__secret:
+            if self.balance >= amount:
+                self.balance -= amount
+                print(f'Your money withdraw is {self.balance}')
+            else:
+                print("Your balance is not enough")
+        else:
+            print("Your secret is not correct. Please, try again!!!")
+
     #payment
     def payment(self, service_type, amount, secret):
         if secret == self.__secret:
@@ -41,13 +52,83 @@ class BankAccount :
         else:
             print("Your secret is not correct. Please, try again!!!")
 
+    #Take loan
+    def takeLoan(self,amount):
+            self.balance += amount
+            print(f'Loan approved! New your balance is {self.balance}')
+
 #create abject
 Thida = BankAccount("Thida", 2000, 246)
-Bopha = BankAccount("Bopha",2500,168)
+# Bopha = BankAccount("Bopha",2500,168)
 
-#ex transfer
-Thida.transfer(Bopha,1000,246)
+#Create class Student_BankAccount
+class Student_BankAccount(BankAccount) :
+    def withdraw(self,secret, Withdraw):
+        if Withdraw <= 499:
+            self.balance -= Withdraw
+            print(f"Your total balance is {self.balance}")
+        else:
+            print("You are a student cannot withdraw 499$up")
+#create abject
+Chantha = Student_BankAccount("Chantha",2000,666)
+Chantha.withdraw(666,660)
+Chantha.checkBalance(666)
 
-#ex checkbalance
-Bopha.chechBalance(168)
+
+#Create class SavingAcc
+class SavingAccount(BankAccount):
+    def calculate_interest(self):
+        self.balance += 10
+        print(f"Your total balance is {self.balance}")
+
+#create object
+Dara = SavingAccount("Dara",1000,132)
+#Dara.checkBalance(132)
+#Dara.deposet(500,132)
+
+#Create class PremiumSaving
+class PremiumSaving(SavingAccount):
+    def deposet(self, amount, secret):
+            self.balance += amount * 0.02
+            print(f'Your total balance is{self.balance}')
+#Create object
+lyly = PremiumSaving("lyly",1000,111)
+
+lyly.deposet(500,111)
+
+# Create class BusinessAccount
+class BusinessAccount(BankAccount):
+    def takeLoan(self, amount):
+        if amount >= 1000:
+            self.balance += (amount - (amount * 0.01))
+            print(f'Loan approved! your balance has accrued interest is {self.balance}')
+        else:
+            self.balance += (amount - (amount * 0.02))
+            print(f'Loan approved! your balance has accrued interest is {self.balance}')
+
+
+
+#create object
+Kaka = BankAccount("Kaka",500,333)
+Kaka.checkBalance(333)
+Kaka.takeLoan(110)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
